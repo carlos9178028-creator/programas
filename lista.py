e5fa1c78b82ec7320313f3ea229abfd0b578ff14
Carlos <carlos9178028@gmail.com>
2025-11-17 21:34:41 -0500

    Funciones

--- lista.py
+++ lista.py
@@ -7,7 +7,18 @@ def  menu():
     print("3. Ver lista de tareas")
     print("4. Salir")
     print("---------------------")
-menu()
+    try:
+      opcion=int(input("Seleccione una opción (1-4): "))
+    except ValueError:
+        print("Error: Debe ingresar un número entero entre 1 y 4.")
+        return menu()
+    else:
+        if opcion < 1 or opcion > 4:
+            print("Error: Opción inválida. Por favor, seleccione una opción entre 1 y 4.")
+            return menu()
+        return opcion
+capturar= menu()
+
 
 
 
